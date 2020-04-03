4f6049df5fbf41ba30784ec2f69ea557107a22e2
lutfiqaraman <lutfiqaraman@gmail.com>
2020-04-04 02:26:24 +0300

    use CORS with the server

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,12 +1,19 @@
-const express = require("express");
 require("dotenv").config({ path: "./config/.env" });
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+let corsOptions = {
+  origin: ["http://localhost:4200", "http://localhost:4000"],
+};
+
+app.use(cors(corsOptions));
 
 const port = process.env.PORT;
 
 require("./routes/app.routes")(app);
 
 app.listen(port, () => {
-    console.log("Server is listening ...");
+  console.log("Server is listening ...");
 });
