0f328d3b8455b4b231adf4565853b112826e7170
lutfiqaraman <lutfiqaraman@gmail.com>
2020-04-05 18:12:19 +0300

    set the migration process

--- BackEnd/seeders/20200405133713-demo-article.js
+++ BackEnd/seeders/20200405133713-demo-article.js
@@ -2,20 +2,22 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('article', [{
-      id: 1,
+    return queryInterface.bulkInsert('articles', [{
+        id: 1,
         title: 'Country Article',
         author: 'Daniel Polo',
         content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
         description: 'An article about the country',
         key: 'water-article',
         date: new Date(),
-        imageUrl: 'https://source.unsplash.com/weekly?country'
+        imageUrl: 'https://source.unsplash.com/weekly?country',
+        createdAt: new Date(),
+        updatedAt: new Date()
     }], {});
     
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('article', null, {});
+    return queryInterface.bulkDelete('articles', null, {});
   }
 };
