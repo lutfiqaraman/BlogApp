47db4dbd16c65e7d8c3fcd2bdfcb31fea7fbf9c9
lutfiqaraman <lutfiqaraman@gmail.com>
2020-04-06 13:36:05 +0300

    get all articles from database

--- BackEnd/controllers/articles.controller.js
+++ BackEnd/controllers/articles.controller.js
@@ -2,10 +2,12 @@
 const fakeArticles = require("../db/fake-articles");
 const { Article } = require("../db/dbSet");
 
+//To Get all the articles form database
 exports.getAllArticles = (req, res) => {
-  Article.findAll().then(articles => res.json(articles));
+  Article.findAll().then((articles) => res.json(articles));
 };
 
+//To Get a single article filtered by the article key
 exports.getAnArticle = (req, res) => {
   res.send(fakeArticles.filter((article) => article.key === req.params.key)[0]);
 };
