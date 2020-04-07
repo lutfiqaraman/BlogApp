86121d6105de054fa60426a1885558159d364137
lutfiqaraman <lutfiqaraman@gmail.com>
2020-04-07 14:52:58 +0300

    edit the comments

--- BackEnd/controllers/articles.controller.js
+++ BackEnd/controllers/articles.controller.js
@@ -1,13 +1,13 @@
-//Controllers of articles
+// Controllers of articles
 const fakeArticles = require("../db/fake-articles");
 const db = require("../db/dbSet");
 
-//To Get all the articles form database
+// To Get all the articles form database
 exports.getAllArticles = async (req, res) => {
   await db.Article.findAll().then((articles) => res.json(articles));
 };
 
-//To Get a single article filtered by the article key
+// To Get a single article filtered by the article key from database
 exports.getAnArticle = async (req, res) => {
   await db.Article.findOne({ where: { key: req.params.key } }).then((article) => {
     res.json(article);
