7782dca6649dc68db4be30124312dcdbab7d1fb1
lutfiqaraman <lutfiqaraman@gmail.com>
2020-04-11 00:59:11 +0300

    edit seed article

--- BackEnd/seeders/20200405133713-demo-article.js
+++ BackEnd/seeders/20200405133713-demo-article.js
@@ -1,30 +1,30 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('articles', [{
-        id: 1,
-        title: 'Country Article',
-        author: 'Daniel Polo',
-        content: 
-          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' + 
-          'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, ' + 
-          'when an unknown printer took a galley of type and scrambled it to make a type specimen book. ' +
-          'It has survived not only five centuries, but also the leap into electronic typesetting, ' +
-          'remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset ' + 
-          'sheets containing Lorem Ipsum passages, and more recently with desktop publishing software ' +
-          'like Aldus PageMaker including versions of Lorem Ipsum.',
-        description: 'An article about the country',
-        key: 'water-article',
-        date: new Date(),
-        imageUrl: 'https://source.unsplash.com/weekly?country',
-        createdAt: new Date(),
-        updatedAt: new Date()
-    }], {});
-    
+    return queryInterface.bulkInsert(
+      "articles",
+      [
+        {
+          id: 1,
+          title: "Seed Article",
+          author: "John Doe",
+          content:
+            "Database seeding is the initial seeding of a database with data. Seeding a database is a process in which an initial set of data is provided to a database when it is being installed. It is especially useful when we want to populate the database with data we want to develop in future." +
+            "This is often an automated process that is executed upon the initial setup of an application. The data can be dummy data or necessary data such as an initial administrator account. ",
+          description: "An article about the seed",
+          key: "seed-article",
+          date: new Date(),
+          imageUrl: "https://source.unsplash.com/weekly?seed",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('articles', null, {});
-  }
+    return queryInterface.bulkDelete("articles", null, {});
+  },
 };
