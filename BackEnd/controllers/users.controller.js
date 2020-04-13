6f75257702cec0a9be58edcbc9c6e03f77e3216e
lutfiqaraman <lutfiqaraman@gmail.com>
2020-04-13 21:35:51 +0300

    edit users controller

--- BackEnd/controllers/users.controller.js
+++ BackEnd/controllers/users.controller.js
@@ -1,18 +1,19 @@
 const db = require("../db/dbSet");
 const crypto = require("crypto");
 
+// Register a user and save it in database
 exports.registerUser = async (req, res) => {
   const name = (req.body.name).toLowerCase();
   const salt = crypto.randomBytes(16).toString("hex");
   const password = req.body.password;
 
-  const passwordHash = crypto
+  const hashedPassword = crypto
     .pbkdf2Sync(password, salt, 1000, 64, "sha512")
     .toString("hex");
 
   await db.User.create({
     name,
-    password: passwordHash,
+    password: hashedPassword,
     salt,
   }).then((result) => res.json(result));
 };
