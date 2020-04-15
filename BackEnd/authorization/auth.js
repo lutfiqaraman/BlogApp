b9d0fb8eeea9b9c75e6ab9aa825d9d7baba5a1bd
lutfiqaraman <lutfiqaraman@gmail.com>
2020-04-16 00:27:39 +0300

    edit auth.js

--- BackEnd/authorization/auth.js
+++ BackEnd/authorization/auth.js
@@ -3,8 +3,7 @@ const tokenManagement = require("./tokenmanagement");
 const auth = async (req, res, next) => {
   const token = req.get("Authorization");
   const verified = tokenManagement.verifyToken(token);
-
-  console.log(token);
+  
   if (!verified) {
     return res.status(401).send("User is not authenticated");
   } 
