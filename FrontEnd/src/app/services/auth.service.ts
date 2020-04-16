7fc167874ba642e1405e27c1a92f243a49288352
lutfiqaraman <lutfiqaraman@gmail.com>
2020-04-16 11:24:37 +0300

    create http-post verb in auth service

--- FrontEnd/src/app/services/auth.service.ts
+++ FrontEnd/src/app/services/auth.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  apiUrl: string = environment.apiUrl;
+  url: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  login(name: string, password: string): Observable<any> {
+    this.url = this.apiUrl + '/user/login';
+    return this.http.post<any>(this.url, {
+      name,
+      password,
+    });
+  }
 }
