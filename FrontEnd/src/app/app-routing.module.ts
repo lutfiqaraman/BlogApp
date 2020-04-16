8039b753bfdef696b00d1da099da4571d72c0c73
lutfiqaraman <lutfiqaraman@gmail.com>
2020-04-16 23:23:40 +0300

    secure the routes

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -11,25 +11,38 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ArticleOverviewComponent } from './components/dashboard/article-overview/article-overview.component';
 import { EditArticleComponent } from './components/dashboard/edit-article/edit-article.component';
 import { LoginComponent } from './components/login/login.component';
+import { RouteguardService } from './services/routeguard.service';
 
 const routes: Routes = [
   { path: 'articles', component: ArticlesComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'dashboard/:id', component: EditArticleComponent },
-  { path: 'dashboard/preview/:id', component: ArticleComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [RouteguardService],
+  },
+  {
+    path: 'dashboard/:id',
+    component: EditArticleComponent,
+    canActivate: [RouteguardService],
+  },
+  {
+    path: 'dashboard/preview/:id',
+    component: ArticleComponent,
+    canActivate: [RouteguardService],
+  },
   { path: 'about', component: AboutComponent },
   { path: 'login', component: LoginComponent },
   { path: '404', component: NotFoundComponent },
   { path: '', component: ArticlesComponent },
   { path: ':id', component: ArticleComponent },
-  { path: '**', component: NotFoundComponent }
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
 export const routingComponent = [
   NavbarComponent,
   FooterComponent,
@@ -39,5 +52,5 @@ export const routingComponent = [
   NotFoundComponent,
   DashboardComponent,
   ArticleOverviewComponent,
-  EditArticleComponent
+  EditArticleComponent,
 ];
--- FrontEnd/src/app/services/routeguard.service.ts
+++ FrontEnd/src/app/services/routeguard.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, UrlTree, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RouteguardService {
+export class RouteguardService implements CanActivate {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+  boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return false;
+  }
 
   constructor() { }
+
 }
